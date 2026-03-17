a580e3c6b522b7667eb699a5521f956f3871a6b7
Umbral Tecno <umbraltecno2025@gmail.com>
2026-03-17 14:46:45 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -6,8 +6,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.VITE_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || "";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
