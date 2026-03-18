2e9a3151e83244aad684e947f716ea80ae8963d3
Umbral Tecno <umbraltecno2025@gmail.com>
2026-03-18 20:59:22 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -15,8 +15,10 @@ require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
 // En Node.js se usa EXCLUSIVAMENTE process.env
-const supabaseUrl = process.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY;
+//const supabaseUrl = process.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
+//const supabaseKey = process.env.VITE_SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("❌ ERROR: No se pudieron cargar las variables de entorno.");
